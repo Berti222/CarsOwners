e3e028b719fdace9d481e39a206329dcc3041eed
Agoston Bertol <agostonbertold@gmail.com>
2020-02-27 18:31:17 +0100

    Feat: add webApi, add OwnersCarsController, add carDto, add car buttons - show data in console

---
+++ CarsOwners/CarsOwners/Controllers/OwnersCarsController.cs
@@ -0,0 +1,52 @@
+﻿using CarsOwners.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarsOwners.Controllers
+{
+    public class OwnersCarsController : ApiController
+    {
+        CarsesOwnerEntities _context = new CarsesOwnerEntities();
+
+        [HttpGet]
+        public IEnumerable<Cars> Index()
+        {
+            return _context.Cars.ToList();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetCars(int id)
+        {
+            IQueryable<SwitchTable> ownerCarsIds = _context.SwitchTable.Where(x => x.OwnersId == id);
+
+            if (ownerCarsIds == null)
+                return NotFound();
+
+            IQueryable<Cars> cars = _context.Cars;
+
+            List<CarDTO> ownerCars = new List<CarDTO>();
+
+            foreach (var item in ownerCarsIds)
+            {
+                Cars car = cars.SingleOrDefault(x => x.Id == item.CarsId);
+
+                if (car != null)
+                {
+                    CarDTO dto = new CarDTO();
+                    dto.Id = item.Cars.Id;
+                    dto.CarBrand = item.Cars.CarBrand;
+                    dto.CarType = item.Cars.CarType;
+                    dto.LicensePlateNumber = item.Cars.LicensePlateNumber;
+                    dto.DateOfProduction = item.Cars.DateOfProduction;
+
+                    ownerCars.Add(dto);
+                }
+            }
+            return Ok(ownerCars);
+        }
+    }
+}
---
+++ CarsOwners/CarsOwners/Models/CarDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarsOwners.Models
+{
+    public class CarDTO
+    {
+        public int Id { get; set; }
+        public string CarBrand { get; set; }
+        public string CarType { get; set; }
+        public string LicensePlateNumber { get; set; }
+        public Nullable<System.DateTime> DateOfProduction { get; set; }
+    }
+}
